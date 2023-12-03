58e5221411e78f8471cdad3cea8a9042150eb2c4
Michael Taggart <mtaggart@taggart-tech.com>
2023-12-03 09:36:25 -0800

    Add install script

---
+++ 2-setup/install-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo gpasswd -a whisper docker
+cd /tmp
+sudo docker compose up -d
+cd -
