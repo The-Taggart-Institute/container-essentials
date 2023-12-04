2e9fb556fd626ad5b1628bdb15c403a4afa64eff
Michael Taggart <mtaggart@taggart-tech.com>
2023-12-03 22:35:10 -0800

    Update script for $USER

--- 2-setup/install-docker.sh
+++ 2-setup/install-docker.sh
@@ -10,4 +10,4 @@ echo \
 	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo gpasswd -a ubuntu docker
+sudo gpasswd -a $USER docker
